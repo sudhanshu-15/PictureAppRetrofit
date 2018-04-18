6a12f58175c077ef80352eb3cc2ed8c4bdcb16cc
Sudhanshu Siddh <ssiddh@iu.edu>
2018-04-18 03:26:58 -0400

    Created retrofit interface and api call

---
+++ app/src/main/java/me/ssiddh/pictureapp/api/PhotoApi.kt
@@ -0,0 +1,10 @@
+package me.ssiddh.pictureapp.api
+
+import me.ssiddh.pictureapp.models.PhotoList
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface PhotoApi {
+    @GET("?key=8727305-46dc7e4a438617f2a0d094f2d&q=bikes&image_type=photo")
+    fun getPhotos() : Call<PhotoList>
+}
---
+++ app/src/main/java/me/ssiddh/pictureapp/api/PhotoRetriver.kt
@@ -0,0 +1,25 @@
+package me.ssiddh.pictureapp.api
+
+import me.ssiddh.pictureapp.models.PhotoList
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PhotoRetriver {
+
+    private val service: PhotoApi
+
+    init {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://pixabay.com/api")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        service = retrofit.create(PhotoApi::class.java)
+    }
+
+    fun getPhotos(callback: Callback<PhotoList>) {
+        val call = service.getPhotos()
+        call.enqueue(callback)
+    }
+}
